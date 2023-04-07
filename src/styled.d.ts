74e43107b1d0a2f37a0e5aa6238bc7189d4f398e
FE012 <twte96@gmail.com>
2023-04-08 01:51:53 +0900

    chore: styled.d.ts 모듈 설정

--- src/styled.d.ts
+++ src/styled.d.ts
@@ -3,7 +3,8 @@ import "styled-components";
 // and extend them!
 declare module "styled-components" {
   export interface DefaultTheme {
-    textColor: string;
     bgColor: string;
+    textColor: string;
+    accentColor: string;
   }
 }
