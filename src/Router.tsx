c406f0aeccce96e8b5c11e90d562444b729dea88
FE012 <twte96@gmail.com>
2023-04-08 02:43:09 +0900

    fix:git page 배포 에러

--- src/Router.tsx
+++ src/Router.tsx
@@ -9,7 +9,7 @@ function Router() {
         <Route path="/:coinId">
           <Coin />
         </Route>
-        <Route path="/">
+        <Route path="/react_crypto-tracker">
           <Coins />
         </Route>
       </Switch>
