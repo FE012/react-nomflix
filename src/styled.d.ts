bbb8a1647a94021733ab51748452e379e9a4fba9
FE012 <twte96@gmail.com>
2023-04-12 17:08:38 +0900

    chore: 테마컬러 box Color 추가

--- src/styled.d.ts
+++ src/styled.d.ts
@@ -4,6 +4,7 @@ import "styled-components";
 declare module "styled-components" {
   export interface DefaultTheme {
     bgColor: string;
+    boxColor: string;
     textColor: string;
     accentColor: string;
   }
