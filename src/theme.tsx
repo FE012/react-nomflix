2750964686668c1f3a9686977067168f57da2bfa
FE012 <twte96@gmail.com>
2023-04-12 17:10:47 +0900

    chore: 스타일 컴포넌트 DefaultTheme 세팅

---
+++ src/theme.tsx
@@ -0,0 +1,8 @@
+import { DefaultTheme } from "styled-components";
+
+export const theme: DefaultTheme = {
+  bgColor: "#222222",
+  boxColor: "#3b3b3b",
+  textColor: "#f5f6fa",
+  accentColor: "#ABD9FF",
+};
