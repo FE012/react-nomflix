8e6f32ea7775ca2b512a1ca71d20437f2d59267a
FE012 <twte96@gmail.com>
2023-04-13 17:45:36 +0900

    chore: 라이트theme 생성

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,8 +1,15 @@
 import { DefaultTheme } from "styled-components";
 
-export const theme: DefaultTheme = {
+export const darkTheme: DefaultTheme = {
   bgColor: "#222222",
   boxColor: "#3b3b3b",
   textColor: "#f5f6fa",
+  accentColor: "#ABD9FF",
+};
+
+export const lightTheme: DefaultTheme = {
+  bgColor: "whitesmoke",
+  boxColor: "white",
+  textColor: "black",
   accentColor: "#ABD9FF",
 };
